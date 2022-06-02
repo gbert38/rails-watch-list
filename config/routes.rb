012512abd87d3b72f3c3fdcb15b17366b10f0d0d
Gregory Bert <gregory_bert@yahoo.com>
2022-06-02 15:00:37 +0200

    models done and crash tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  # # A user can see all the lists
+  # get "/lists", to: "lists#index"
+  # # A user can see the details of a given list and its name
+  # get "lists/42", to: "lists#new"
+  # # A user can create a new list
+  # get "lists/new", to: "lists#show"
+  # post "lists", to:"lists#create"
 end
